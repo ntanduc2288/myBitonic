3414e33c71e9405e0c954309e1cb86acb1876e1a
Nguyen Tan Duc <ntanduc2288@gmail.com>
2018-04-10 17:24:42 -0700

    Add SelectionListReducer

---
+++ src/redux/reducers/SelectionListReducer.js
@@ -0,0 +1,90 @@
+import Constants from 'src/constants/Constants';
+import RandomWords from 'random-words';
+
+function initParentItem(id, mainName, datas){
+    return {
+        id,
+        mainName,
+        datas,
+    }
+}
+
+function initDefaultList() {
+    wordList = Constants.wordList.slice(0);
+    newList = [];
+    wordList.forEach(element => {
+        item = initParentItem(element.id, element.name, []);
+        newList.push(item);
+    });
+
+    test = [];
+    newList.forEach(element => {
+
+        words = RandomWords(11);
+        words.filter(word => element.mainName != word);
+
+        let nextIndex = wordList.length;
+        words = words.map(function(word, i) {
+            nextIndex++;
+            return {
+                id: nextIndex,
+                name: word,
+            }
+        });
+
+        test = words;
+
+        words.push({
+            id: element.id,
+            name: element.mainName,
+        })
+
+        words.sort((a, b) => {
+            return a.name > b.name ? 1 : -1;
+        })
+
+        element.datas = words;
+        
+
+    });
+
+    // alert(JSON.stringify(newList))
+
+    return newList;
+
+}
+
+const defaultState = {
+    parentList: initDefaultList(),
+    childList: [],
+}
+
+function getNewStateById(state, selectedId) {
+    let newChildList = [];
+    for (let i = 0; i < state.parentList.length; i++) {
+        if (state.parentList[i].id === selectedId) {
+            newChildList = state.parentList[i].datas;
+            break;
+        }
+    }
+    
+    return {
+        ...state,
+        childList: newChildList,
+    }
+}
+
+function Reducer(state = defaultState, action) {
+    switch (action.type) {
+        case Constants.CHANGE_SELECTION_LIST:
+            return getNewStateById(state, action.id);
+        default:
+            return state;
+    }
+
+    return state;
+}
+
+
+
+export default Reducer;
